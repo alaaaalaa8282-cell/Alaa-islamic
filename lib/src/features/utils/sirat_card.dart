671853656d2f041f14eb81d8d564d9c9eb5f28b3
alaaaalaa8282-cell <alaaaalaa82.82@gmail.com>
2026-04-29 20:26:25 +0300

    Fix import path for constants in sirat_card.dart

--- lib/src/features/utils/sirat_card.dart
+++ lib/src/features/utils/sirat_card.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import '../../../core/util/constants.dart';
+import '../../core/util/constants.dart';
 
 class SiratCard extends StatelessWidget {
   const SiratCard({required this.child, this.padding, this.margin});
